705ca5baff45884b20783d7b314a824932634bf9
Joselin Grez <joselingrez.m@hotmail.es>
2017-09-26 17:12:10 -0300

    Mejorando estilos

---
+++ assets/js/js/main.js
@@ -0,0 +1,16 @@
+var nombresito = "Diego";
+var dias = [
+  "Lunes",
+  "Martes",
+  "Miercole",
+  "Jueves",
+  "Viernes",
+  "Sabado",
+  "Domingo"
+]
+
+
+for (var i = 0; i < dias.length; i++) {
+	console.log(`El dia ${i} es el ${dias[i]}`)	
+
+}
